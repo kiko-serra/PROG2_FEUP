190da8bf983e9c31fb091c800114a3017de7c689
kikoserra <franciscopimentelserra@gmail.com>
2021-03-08 18:55:03 +0000

    commit

---
+++ P02/ex2.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "vetor.h"
+#define NMAX 80
+
+int main(){
+    int i, a;
+    char str[NMAX];
+    vetor *v = vetor_novo();
+    printf("Vetor novo criado\n");
+    if (v==NULL)
+    {
+        printf("Vetor não criado\n");
+        return 0;
+    }
+    
+    //Solicita 5 frases com máximo de 80 caracteres e guarda em v
+    for (i = 0; i < 5; i++)
+    {
+        printf("Insira a %dº frase: ", i+1); fgets(str, NMAX, stdin); str[strlen(str)-1]='\0';
+        vetor_insere(v, str, i);
+    }
+    //imprime as 5 strings do vetor
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%s\n", vetor_elemento(v, i));
+    }
+    printf("Escreva uma frase: "); fgets(str, NMAX, stdin); str[strlen(str)-1]='\0';
+    //se a frase ja existir no vetor apagar essa frase do vetor
+    a=vetor_pesquisa(v, str);
+    if(a!=-1){
+        vetor_remove(v, a);
+    }
+    //imprimir o vetor
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%s\n", vetor_elemento(v, i));
+    }
+    //ordenar o vetor por ordem alfabetica
+    vetor_ordena(v);
+    //imprimir o vetor
+    printf("Vetor ordenado por ordem alfabética: ");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%s\n", vetor_elemento(v, i));
+    }
+
+    return 0;
+}
--- P02/vetor.c
+++ P02/vetor.c
@@ -1,20 +1,21 @@
 /*****************************************************************/
-/*          Biblioteca vetor | PROG2 | MIEEC | 2018/19          */
+/*          Biblioteca vetor | PROG2 | MIEEC | 2020/21          */
 /*****************************************************************/
 
 #include "vetor.h"
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
-vetor* vetor_novo()
+vetor *vetor_novo()
 {
-	vetor* vec;
+	vetor *vec;
 
 	/* aloca memoria para a estrutura vetor */
-	vec = (vetor*)malloc(sizeof(vetor));
-	if(vec == NULL)
-	return NULL;
+	vec = (vetor *)malloc(sizeof(vetor));
+	if (vec == NULL)
+		return NULL;
 
 	vec->tamanho = 0;
 	vec->capacidade = 0;
@@ -23,12 +24,12 @@ vetor* vetor_novo()
 	return vec;
 }
 
-void vetor_apaga(vetor* vec)
+void vetor_apaga(vetor *vec)
 {
 	int i;
 
-	if(vec == NULL)
-	return;
+	if (vec == NULL)
+		return;
 
 	/* liberta memoria de cada string */
 	for (i = 0; i < vec->tamanho; i++)
@@ -43,21 +44,21 @@ void vetor_apaga(vetor* vec)
 	free(vec);
 }
 
-int vetor_atribui(vetor* vec, int pos, const char* valor)
+int vetor_atribui(vetor *vec, int pos, const char *valor)
 {
 	unsigned int len_valor;
 
 	if (vec == NULL || pos < 0 || pos >= vec->tamanho)
-	return -1;
+		return -1;
 
 	len_valor = strlen(valor);
 
 	/* reserva mais espaco se necessario */
-	if(len_valor > strlen(vec->elementos[pos].str))
+	if (len_valor > strlen(vec->elementos[pos].str))
 	{
-		vec->elementos[pos].str = (char*)realloc(vec->elementos[pos].str, (len_valor+1)*sizeof(char));
-		if(vec->elementos[pos].str == NULL)
-		return -1;
+		vec->elementos[pos].str = (char *)realloc(vec->elementos[pos].str, (len_valor + 1) * sizeof(char));
+		if (vec->elementos[pos].str == NULL)
+			return -1;
 	}
 
 	/* copia valor */
@@ -66,56 +67,56 @@ int vetor_atribui(vetor* vec, int pos, const char* valor)
 	return pos;
 }
 
-int vetor_tamanho(vetor* vec)
+int vetor_tamanho(vetor *vec)
 {
-	if(vec == NULL)
-	return -1;
+	if (vec == NULL)
+		return -1;
 
 	return vec->tamanho;
 }
 
-const char* vetor_elemento(vetor* vec, int indice)
+const char *vetor_elemento(vetor *vec, int indice)
 {
 	if (vec == NULL || indice < 0 || indice >= vec->tamanho)
-	return NULL;
+		return NULL;
 
 	return vec->elementos[indice].str;
 }
 
-int vetor_insere(vetor* vec, const char* valor, int pos)
+int vetor_insere(vetor *vec, const char *valor, int pos)
 {
 	int i, n;
 
-	if(vec == NULL || pos < -1 || pos > vec->tamanho)
+	if (vec == NULL || pos < -1 || pos > vec->tamanho)
 		return -1;
 
 	/* aumenta elementos do vetor se capacidade nao for suficiente */
-	if(vec->tamanho == vec->capacidade)
+	if (vec->tamanho == vec->capacidade)
 	{
-		if(vec->capacidade == 0)
-		vec->capacidade = 1;
+		if (vec->capacidade == 0)
+			vec->capacidade = 1;
 		else
-		vec->capacidade *= 2;
+			vec->capacidade *= 2;
 
-		vec->elementos = (v_elemento*)realloc(vec->elementos, vec->capacidade * sizeof(v_elemento));
-		if(vec->elementos == NULL)
-		return -1;
+		vec->elementos = (v_elemento *)realloc(vec->elementos, vec->capacidade * sizeof(v_elemento));
+		if (vec->elementos == NULL)
+			return -1;
 	}
 
 	/* se pos=-1 insere no fim do vetor */
-	if(pos == -1)
+	if (pos == -1)
 		pos = vec->tamanho;
 
 	/* copia todos os elementos a partir da posicao pos ate' ao fim do vetor para pos+1 */
-	for(i=vec->tamanho-1; i>=pos; i--)
+	for (i = vec->tamanho - 1; i >= pos; i--)
 	{
-		vec->elementos[i+1] = vec->elementos[i];
+		vec->elementos[i + 1] = vec->elementos[i];
 	}
 
 	/* aloca espaco para a nova string na posicao pos */
-	vec->elementos[pos].str = (char*)calloc(strlen(valor)+1, sizeof(char));
-	if(vec->elementos[pos].str == NULL)
-	return -1;
+	vec->elementos[pos].str = (char *)calloc(strlen(valor) + 1, sizeof(char));
+	if (vec->elementos[pos].str == NULL)
+		return -1;
 
 	/* copia valor */
 	strcpy(vec->elementos[pos].str, valor);
@@ -125,20 +126,20 @@ int vetor_insere(vetor* vec, const char* valor, int pos)
 	return pos;
 }
 
-int vetor_remove(vetor* vec, int pos)
+int vetor_remove(vetor *vec, int pos)
 {
 	int i;
 
-	if(vec == NULL || pos < 0 || pos >= vec->tamanho)
-	return -1;
+	if (vec == NULL || pos < 0 || pos >= vec->tamanho)
+		return -1;
 
 	/* liberta string na posicao a remover */
 	free(vec->elementos[pos].str);
 
 	/* copia todos os elementos a partir da posicao pos+1 ate' ao fim do vetor para pos */
-	for(i=pos+1; i<vec->tamanho; i++)
+	for (i = pos + 1; i < vec->tamanho; i++)
 	{
-		vec->elementos[i-1] = vec->elementos[i];
+		vec->elementos[i - 1] = vec->elementos[i];
 	}
 
 	vec->tamanho--;
@@ -146,41 +147,89 @@ int vetor_remove(vetor* vec, int pos)
 	return 0;
 }
 
-int vetor_pesquisa(vetor* vec, const char* valor)
+int vetor_pesquisa(vetor *vec, const char *valor)
 {
 	int i;
 
-	if(vec == NULL)
-	return -1;
+	if (vec == NULL)
+		return -1;
 
 	/* pesquisa sequencial */
 	for (i = 0; i < vec->tamanho; i++)
 	{
 		if (strcmp(vec->elementos[i].str, valor) == 0)
-		return i;
+			return i;
 	}
 
 	return -1;
 }
 
-int vetor_ordena(vetor* vec)
+int vetor_ordena(vetor *vec)
 {
 	int i, j;
 	char *tmp;
 
-	if(vec == NULL)
-	return -1;
+	if (vec == NULL)
+		return -1;
 
 	/* ordenacao por insercao */
 	for (i = 1; i < vec->tamanho; i++)
 	{
 		tmp = vec->elementos[i].str;
-		for (j = i; j > 0 && strcmp(tmp, vec->elementos[j-1].str) < 0; j--)
+		for (j = i; j > 0 && strcmp(tmp, vec->elementos[j - 1].str) < 0; j--)
 		{
-			vec->elementos[j] = vec->elementos[j-1];
+			vec->elementos[j] = vec->elementos[j - 1];
 		}
 		vec->elementos[j].str = tmp;
 	}
 
 	return 0;
+}
+//cria um novo vetor que resulta da concatenação de dois outros vetores
+vetor *vetor_concatena(vetor *vec1, vetor *vec2)
+{
+	vetor *v = vetor_novo();
+	for (int i = 0; i < vetor_tamanho(vec1); i++)
+	{
+		vetor_insere(v, vetor_elemento(vec1, i), -1);
+	}
+	for (int i = 0; i < vetor_tamanho(vec2); i++)
+	{
+		vetor_insere(v, vetor_elemento(vec2, i), -1);
+	}
+
+	return v;
+}
+//inverte os elementos do vetor
+int vetor_inverte(vetor *vec)
+{
+	char a[100] = {};
+	int l = 4;
+	for (int i = 0; i < 5; i++)
+	{
+		if (i < l)
+		{
+			strcpy(a, vec->elementos[i].str);
+			strcpy(vec->elementos[i].str, vec->elementos[l].str);
+			strcpy(vec->elementos[l].str, a);
+			l--;
+		}
+		else
+			break;
+	}
+	return 0;
+}
+//cria um novo vetor com os mesmos elementos do vetor vec mas guardados emposições aleatórias
+vetor *vetor_baralha(vetor *vec)
+{
+	srand(time(NULL));
+	int r;
+	vetor *v;
+	for (int i = 0; i < 5; i++)
+	{
+		r = rand() % 5;
+		v->elementos[r].str = vec->elementos[i].str;
+	}
+
+	return v;
 }
