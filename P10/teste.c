39eb4a2a4d259e48d1bdca48388639af10503048
kikoserra <franciscopimentelserra@gmail.com>
2021-05-17 18:05:57 +0100

    commit

--- P10/teste.c
+++ P10/teste.c
@@ -6,35 +6,49 @@
 #include "lista.h"
 
 int main(){
+    //1.1
     clock_t inicio, fim;
     double tempo;inicio = clock();
         char string[100];
         tabela_dispersao *tab= tabela_nova(100000, hash_djbm);
         FILE *f = fopen("englishwords.txt", "r");
-        do
+        while (fgets(string, 100, f)!=NULL)
         {
+            string[strlen(string)-1]='\0';
             tabela_adiciona(tab, string, string);
-        } while (fgets(string, 100, f)!=NULL);
+        } 
     fim = clock();
     tempo = (double)(fim - inicio) / CLOCKS_PER_SEC;
-    printf("tempo em segundos: %lf\n", tempo);
+    printf("tempo em segundos de inserir por tabela: %lf\n", tempo);
 
-    fseek(f, 0, SEEK_SET);
+    fseek(f, 0, SEEK_SET); // volta ao inicio do ficheiro
+
     inicio = clock();
         lista *lst = lista_nova();
         char string1[100];
-        do
-        {
-            strlen
+        while (fgets(string1, 100, f)!=NULL)
+            {
+            string1[strlen(string1)-1]='\0';
             lista_insere(lst, string1, NULL);
-        } while (fgets(string1, 100, f)!=NULL);
+        } 
         
     fim = clock();
     tempo = (double)(fim - inicio) / CLOCKS_PER_SEC;
-    printf("tempo em segundos: %lf\n", tempo);
-    
-    
-    
+    printf("tempo em segundos de inserir por lista: %lf\n", tempo);
+
+    fseek(f, 0, SEEK_SET); // volta ao inicio do ficheiro
+
+    int n;
+    objeto *ob= tabela_elementos(tab, &n);
+    int val;
+    val= rand() % n; // gera um elemento aleatorio
+    inicio = clock();
+        tabela_valor(tab, ob[val].chave);
+        
+    fim = clock();
+    tempo = (double)(fim - inicio) / CLOCKS_PER_SEC;
+    printf("tempo em segundos de pesquisar na tabela: %lf\n", tempo);
+
 
 
 }
